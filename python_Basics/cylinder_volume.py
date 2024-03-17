ddb3424ae9e42946d5d74c59f10c7d27f1bf76b8
sujoydeb121 <sujoydebcoder121@gmail.com>
2024-03-17 14:51:31 +0100

    Create cylinder_volume.py

---
+++ python_Basics/cylinder_volume.py
@@ -0,0 +1,8 @@
+def find_cylinder_volume(radius=5, height=10):
+    cylinder_volume = 3.14 * (radius ** 2) * height
+    return cylinder_volume
+
+
+r = 13
+h = 15
+print(find_cylinder_volume(r, h))
